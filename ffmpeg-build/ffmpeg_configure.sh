9a4dc6affc1ba28acfca74e635a5c7fc55e3e678
dengyuhan <464958567@qq.com>
2018-06-01 19:08:35 +0800

    优化命令

--- ffmpeg-build/ffmpeg_configure.sh
+++ ffmpeg-build/ffmpeg_configure.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
-unzip ./ffmpeg-build/ffmpeg-3.2.9.zip
+#如果没有ffmpeg文件夹 就解压
+if [ ! -d ./ffmpeg-jni/ffmpeg-3.2.9 ]; then
+  unzip ./ffmpeg-build/ffmpeg-3.2.9.zip -d ./ffmpeg-jni
+fi
+
+#复制编译脚本到ffmpeg文件夹
 cp ./ffmpeg-build-script/build_android.sh ./ffmpeg-jni/ffmpeg-3.2.9/build_android.sh
+
 cd ./ffmpeg-jni/ffmpeg-3.2.9
 ./configure --disable-yasm
 ./build_android.sh
