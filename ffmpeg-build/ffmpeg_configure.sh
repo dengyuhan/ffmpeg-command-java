80865492fff2db6e44aa06e87ad807f8faee161c
dengyuhan <464958567@qq.com>
2018-06-04 10:41:43 +0800

    调整命令行

--- ffmpeg-build/ffmpeg_configure.sh
+++ ffmpeg-build/ffmpeg_configure.sh
@@ -5,8 +5,10 @@ if [ ! -d ./ffmpeg-jni/ffmpeg-3.2.9 ]; then
 fi
 
 #复制编译脚本到ffmpeg文件夹
-cp ./ffmpeg-build-script/build_android.sh ./ffmpeg-jni/ffmpeg-3.2.9/build_android.sh
+cp ./ffmpeg-build/build_android.sh ./ffmpeg-jni/ffmpeg-3.2.9/build_android.sh
 
-cd ./ffmpeg-jni/ffmpeg-3.2.9
+cd ./ffmpeg-jni
+chmod -R +x ffmpeg-3.2.9
+cd ./ffmpeg-3.2.9
 ./configure --disable-yasm
 ./build_android.sh
--- ffmpeg-jni/src/main/cpp/com_dyhdyh_ffmpeg_FFmpegJNI.c
+++ ffmpeg-jni/src/main/cpp/com_dyhdyh_ffmpeg_FFmpegJNI.c
@@ -2,15 +2,14 @@
 #include "com_dyhdyh_ffmpeg_FFmpegJNI.h"
 
 JNIEXPORT jint JNICALL Java_com_dyhdyh_ffmpeg_FFmpegJNI_nativeExec
-        (JNIEnv *env, jclass jcls, jobjectArray commands){
-    /*int argc = (*env)->GetArrayLength(env, commands);
+        (JNIEnv *env, jclass jcls, jobjectArray commands) {
+    int argc = (*env)->GetArrayLength(env, commands);
     char *argv[argc];
 
     int i;
     for (i = 0; i < argc; i++) {
         jstring js = (jstring) (*env)->GetObjectArrayElement(env, commands, i);
-        argv[i] = (char*) (*env)->GetStringUTFChars(env, js, 0);
+        argv[i] = (char *) (*env)->GetStringUTFChars(env, js, 0);
     }
-    return run(argc, argv);*/
-    return 0;
+    return run(argc, argv);
 }
